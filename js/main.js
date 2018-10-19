71a5a4ef91969ca0e90409f6ab2152fd6018c88b
fyzm <fangyuan931018@gmail.com>
2018-10-19 19:44:57 +0800

    add some thing resume with information

--- js/main.js
+++ js/main.js
@@ -1,40 +1,133 @@
 
-// The code snippet you want to highlight, as a string
+//把code写到#code和style标签里
+function writeCss(prefix,code,fn){
+  let domCode = document.querySelector('#code')
+  let n = 0
+  let id = setInterval(()=>{
+    n += 1
+    domCode.innerHTML = Prism.highlight(prefix + code.substring(0,n),Prism.languages.css);
+    styleTag.innerHTML = prefix + code.substring(0,n)
+    domCode.scrollTop = domCode.scrollHeight
+    if(n >=code.length){
+      window.clearInterval(id)
+      fn && fn.call()
+    }
+  },70) 
+}
 
-var text = "html{color:red;}";
+function writeMarkdown(markdown,fn){
+  let domPaper = document.querySelector('#paper>.content')
+  let n = 0
+  let id = setInterval(()=>{
+    n += 1
+    domPaper.innerHTML = markdown.substring(0,n)
+    domPaper.scrollTop = domPaper.scrollHeight
+    if(n >=markdown.length){
+      window.clearInterval(id)
+      fn && fn.call()
+    }
+  },35)
+}
 
-// Returns a highlighted HTML string
-var html = Prism.highlight(text, Prism.languages.html);
-console.log(html)
-
-var result = `/*
-*面试官你好，我是XXX
-*我将以动画的形式来介绍我自己
-*只用文字介绍太单调了
-*我就用代码来介绍吧
-*首先准备一些样式
+var css1 = `/* 
+* 面试官你好，我是FY
+* 只用文字作做我介绍太单调了
+* 我就用代码来介绍吧
+* 首先准备一些样式
 */
 *{
-  transition:all 1s;
+ transition: all 1s;
 }
 html{
-  background:rgb(222,222,222);
-  font-size:16px;
+ background: #eee;
 }
 #code{
-  border:1px solid red;
-  padding:16px;
+ border: 1px solid #aaa;
+ padding: 16px;
+}
+/* 我需要一点代码高亮 */
+.token.selector{ color: #690; }
+.token.property{ color: #905; }
+/* 加一个呼吸效果 */
+#code{
+ animation: breath 0.5s infinite alternate-reverse;
+}
+/* 现在正式开始 */
+/* 我需要一张白纸 */
+#code-wrapper{
+ width: 50%; left: 0; position: fixed; 
+ height: 100%;
+}
+#paper > .content {
+display: block;
+}
+/* 于是我就可以在白纸上写字了，请看右边 */
+`
+var css2 = `
+/* 接下来用一个优秀的库 marked.js
+ * 把 Markdown 变成 HTML
+ */
+`
+var md = `
+# 自我介绍
+我叫 FY
+重庆大学计算机专业毕业
+自学前端
+希望应聘前端开发岗位
+# 技能介绍
+熟悉 JavaScript CSS 
+# 项目介绍
+1. APPle 轮播
+2. 在线 简历
+3. Canavs 画板
+# 联系方式
+- QQ 1773319368
+- Email fangyuan931018@gmail.com
+- 手机 xxxxxxx
+`
+let css3 = `
+/*
+ * 这就是我的会动的简历
+ * 谢谢观看
+ */
+`
+
+
+
+
+writeCss('',css1,()=>{ //witerCss call the function
+  createPaper(()=> {
+    writeMarkdown(md,()=>{
+      writeCss(css1,css2,()=>{
+        convertMarkdownToHtml(()=>{
+          writeCss(css1+css2,css3,()=>{
+            console.log('完成')
+          })
+        })
+      })
+    })
+  })
+})
+
+
+
+
+function createPaper(fn){
+  var paper = document.createElement('div')
+  paper.id = 'paper'
+  var content = document.createElement('pre')
+  content.className = 'content'
+  paper.appendChild(content)
+  document.body.appendChild(paper)
+  fn && fn.call()
 }
 
-`
-var n = 0
-var id = setInterval(()=>{
-  n +=1
-  code.innerHTML = result.substring(0,n)
-  code.innerHTML = code.innerHTML.replace('html','<span style="color:red;">html</span>')
-  styleTag.innerHTML = result.substring(0,n)
-  console.log('一轮')
-  if(n >=result.length){
-    window.clearInterval(id)
-  }
-},10)
+
+function convertMarkdownToHtml(fn){
+  var div = document.createElement('div')
+  div.className = 'html markdown-body'
+  div.innerHTML = marked(md)
+  let markdownContainer = document.querySelector('#paper > .content')
+  markdownContainer.replaceWith(div)
+  fn && fn.call()
+}
